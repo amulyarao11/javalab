6e982f8ff070e76b7a2cc7b966693a65ec580ec6
amulyarao11 <63998658+amulyarao11@users.noreply.github.com>
2020-04-20 12:31:44 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,40 @@
+package javalab;
+public class Employee 
+{
+	String name;
+	int age;
+	String dept;
+	double sal;
+	public String getName() 
+	{
+		return name;//returns employee name
+	}
+	public void setName(String name) 
+	{
+		this.name = name;//sets employee name
+	}
+	public int getAge() 
+	{
+		return age;//returns employee age
+	}
+	public void setAge(int age) 
+	{
+		this.age = age;//sets employee age
+	}
+	public String getDept() 
+	{
+		return dept;//returns employee department
+	}
+	public void setDept(String dept) 
+	{
+		this.dept = dept;//sets employee department
+	}
+	public double getSal() 
+	{
+		return sal;//returns employee salary
+	}
+	public void setSal(double sal) 
+	{
+		this.sal = sal;//sets employee salary
+	}
+}
